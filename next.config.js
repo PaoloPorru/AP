ac7e3069ad5e8181b9898559c150185f70ee915b
pporru <paolo.porru@jakala.com>
2025-09-03 16:10:04 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,23 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { 
-    images: 
-    { 
-        remotePatterns: 
-        [ 
-            { 
-                protocol: 'https', 
-                hostname: '**' 
-            }, { 
-                protocol: 'http', 
-                hostname: '**' 
-            }
-        ] 
-    }, 
-    experimental: 
-    { 
-        serverActions: 
-        { 
-            bodySizeLimit: '2mb' } 
-        } 
-    };
+const nextConfig = {
+  output: 'export',        // <-- genera sito statico
+  distDir: 'dist',         // opzionale, se vuoi mantenere la cartella dist
+  images: { 
+    remotePatterns: [ 
+      { protocol: 'https', hostname: '**' }, 
+      { protocol: 'http', hostname: '**' } 
+    ] 
+  },
+  experimental: { 
+    serverActions: { bodySizeLimit: '2mb' } 
+  }
+};
+
 module.exports = nextConfig;
