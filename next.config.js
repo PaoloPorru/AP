fedf777f0a776fad1790509407e72fac1ad2dbef
pporru <paolo.porru@jakala.com>
2025-09-03 15:59:32 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig={images:{remotePatterns:[{protocol:'https',hostname:'**'},{protocol:'http',hostname:'**'}]},experimental:{serverActions:{bodySizeLimit:'2mb'}}};
-module.exports=nextConfig;
+const nextConfig = { 
+    distDir: 'dist',
+    images: 
+    { 
+        remotePatterns: 
+        [ 
+            { 
+                protocol: 'https', 
+                hostname: '**' 
+            }, { 
+                protocol: 'http', 
+                hostname: '**' 
+            }
+        ] 
+    }, 
+    experimental: 
+    { 
+        serverActions: 
+        { 
+            bodySizeLimit: '2mb' } 
+        } 
+    };
+module.exports = nextConfig;
